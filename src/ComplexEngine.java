2cb16955b90eccd3234613fc50d3d08548c40019
William <williamd8323@afsenyc.org>
2018-03-17 15:39:10 -0400

    levels class

--- src/ComplexEngine.java
+++ src/ComplexEngine.java
@@ -1,2 +1,15 @@
+import java.util.*;
+import java.util.List;
+
 public class ComplexEngine {
+    private Player player;
+    private List<Location> locationsInGame;
+    private Map<String, Location> hashedLocations;
+    private Map<String, Location> emptyMap;
+    private Location levels;
+    private Game curGame;
+    private String nextLocationName;
+
+
+
 }
