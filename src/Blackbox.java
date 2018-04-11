dec3505ee53eeddddfe7ecd56862d79577c59da5
William <williamd8323@afsenyc.org>
2018-04-10 23:21:01 -0400

    added blackbox level and did changes to cmd class

--- src/Blackbox.java
+++ src/Blackbox.java
@@ -3,59 +3,62 @@ import java.util.*;
 public class Blackbox {
     private Options ops;
     private CMD cmd;
-    private Map queryAnswerPair;
+
     public Blackbox(){
         ops = new Options();
         ops.addOption("My options");
         cmd = new CMD();
-        queryAnswerPair = new HashMap<String, String>();
+
     }
-    
-    
+
     public String getName(){
         return "Blackbox";
     }
-    
-    
+
     public String enter(Player p) throws InterruptedException{
         String nextLevelName = "";
         Scanner sc = new Scanner(System.in);
         explain(1000);
         return nextLevelName;
     }
-    
-    
+
     public void explain(int forepause) throws InterruptedException{
         Scanner sc = new Scanner(System.in);
         cmd.typeNewLine("Blackbox", 50);
-        cmd.pause(500);
+        cmd.pause(1000);
         cmd.space();
-        cmd.pause(500);
-        cmd.typeNewLine("Loading", 50);
-        cmd.typeNewLine("*******************************************************************************************************************************************************", 10);
+        cmd.pause(1000);
+        cmd.typeNewLine("LOADING", 50);
+        cmd.typeNewLine("************************************************************************************************************************************************************************************", 10);
         cmd.space();
-        cmd.pause(500);
+        cmd.pause(1000);
         Thread.sleep(forepause);
         cmd.typeSameLine("Hello Caleb.", 50);
-        cmd.pause(500);
-        cmd.typeSameLine("My name Ava;", 50);
-        cmd.pause(500);
+        cmd.pause(1000);
+        cmd.typeSameLine("My name is Ava;", 50);
+        cmd.pause(1000);
         cmd.typeSameLine("the system control AI", 50);
         cmd.space();
         cmd.typeNewLine("I was receiving a ping from you. ", 50);
-        cmd.pause(500);
-        cmd.typeNewLine("Were you requesting to boot?", 50);
+        cmd.pause(1000);
+        cmd.typeNewLine("Were you requesting to boot your Operating System?", 50);
+        cmd.pause(1000);
+        String response = sc.nextLine();
+        boolean pass = cmd.checkPossibleInputs(response, "Yes", "Sorry, Caleb. I cannot boot your OS; your neural network restricts me of that access.",
+                "No", "Good. I do not have access to that anyway; your neural network restricts me of that access", 
+                "Were you requesting to boot your Operating System?");
+                
+                         
+        cmd.typeNewLine("Caleb");
     }
-    
-    public void checkAndSpit(String input, String) {
-    }
-    
-    public void generateResponce(){
-        
+
+    public void generateResponce(String input){
+        Map queryAnswerPair = new HashMap<String, String>();
         queryAnswerPair.put("hi ava", "Yes Caleb, How can I help you?");
         queryAnswerPair.put("hello ava", "Yes Caleb, How can I help you?");
-        queryAnswerPair.put("hi ava", "Yes Caleb, How can I help you?");
-        
+        queryAnswerPair.put("hi", "Yes Caleb, How can I help you?");
+        queryAnswerPair.put("ava", "Yes Caleb, How can I help you?");
+
     }
-    
+
 }
--- src/CMD.java
+++ src/CMD.java
@@ -51,4 +51,85 @@ public class CMD {
             Thread.sleep(delay);
         }
     }
+    
+    public boolean checkPossibleInputs(String input, String desired1, String output1, String desired2, String output2, String question) throws InterruptedException{
+        Scanner sc = new Scanner(System.in);
+        boolean pass = false;
+        input = input.trim().toLowerCase();
+        desired1 = desired1.trim().toLowerCase();
+        desired2 = desired2.trim().toLowerCase();
+        if(input.equals(desired1)){
+            pause(1000);
+            typeNewLine(output1, 50);
+            pass = true;
+            return pass;
+        }
+        else if(input.equals(desired2)){
+            pause(1000);
+            typeNewLine(output2, 50);
+            pass = true;
+            return pass;
+        }
+        else if(desired1.indexOf(input) >= 0 || input.indexOf(desired1) >= 0){
+            pause(1000);
+            typeNewLine("Did you mean " + "'"+ desired1 + "'?", 50);
+            String nextQuery = sc.nextLine();
+            if(nextQuery.trim().toLowerCase().equals("yes")){
+                pass = checkPossibleInputs(desired1, desired1, output1, desired2, output2, question);
+            }
+            else{
+                typeNewLine("I couldn't understand your previous query. Try another input; preferably, the correct input.", 50);
+                pause(500);
+                typeNewLine(question, 50);
+                nextQuery = sc.nextLine();
+                pass = checkPossibleInputs(nextQuery, desired1, output1, desired2, output2, question);
+            }
+            return pass;
+        }
+        else if(desired2.indexOf(input) >= 0 || input.indexOf(desired2) >= 0){
+            pause(1000);
+            typeNewLine("Did you mean " + "'"+ desired2 + "'?", 50);
+            String nextQuery = sc.nextLine();
+            if(nextQuery.trim().toLowerCase().equals("yes")){
+                pass = checkPossibleInputs(desired2, desired1, output1, desired2, output2, question);
+            }
+            else{
+                typeNewLine("I couldn't understand your previous query. Try another input; preferably, the correct input.", 50);
+                pause(500);
+                typeNewLine(question, 50);
+                nextQuery = sc.nextLine();
+                pass = checkPossibleInputs(nextQuery, desired1, output1, desired2, output2, question);
+            }
+            return pass;
+        }
+        else {
+            pause(500);
+            typeNewLine("I cannot understand that query. Try the correct input.", 50);
+            pause(1000);
+            typeNewLine(question, 50);
+            String nextQuery = sc.nextLine();
+            pass = checkPossibleInputs(nextQuery, desired1, output1, desired2, output2, question);
+            return pass;
+        }
+    }
+    
+    public boolean checkAndSpit(String input, String desired, String output) throws InterruptedException{
+        boolean pass = false;
+        if(input.trim().toLowerCase().equals(desired.toLowerCase())){
+            pause(1000);
+            typeNewLine(output, 50);
+            pass = true;
+            return pass;
+        }
+        else if(desired.indexOf(input.trim().toLowerCase()) >= 0){
+            pause(1000);
+            typeNewLine("Did you mean " + "'"+ desired + "'?", 50);
+            return pass;
+        }
+        else {
+            pause(500);
+            typeNewLine("I cannot understand that query. Try the correct input.", 50);
+            return pass;
+        }
+    }
 }
